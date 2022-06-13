09fb10ba5fbecb66a00a1f7291c3a875e1be5799
PedroDamasco <103971452+PedroDamasco@users.noreply.github.com>
2022-06-13 20:10:10 -0300

    Update Exercicio vetores A e B.java

--- Exercicio vetores A e B.java
+++ Exercicio vetores A e B.java
@@ -4,7 +4,8 @@ public class Exercicio7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		
+		// criacao dos valores e vetores.
 		int aa,bb,ca=0,cb=0;
 		int A[]= {4,6,7,1,0};
 		int B[]= {7,1,3,1,2};
@@ -12,12 +13,15 @@ public class Exercicio7 {
 		int BC[]= {};
 		int res=0;
 		
+		// contando tamanho dos vertores
 		bb =B.length;
 		aa =A.length;
 
+		// criar informar numero de colunas.
 		System.out.print("numero de colunas: "+aa);
 		System.out.print("numero de colunas: "+bb);	
 		
+		// Criar condicoes para primeira condicao
 		for (int a=0;a<aa;a++)
 			if(A[a]>4)
 			System.out.printf("\n Numero A nessa possicao: "+A[a]);
@@ -25,19 +29,22 @@ public class Exercicio7 {
 			AC[ca]=A[a];
 			
 			System.out.print("\n");
-			
+
+		// Criando condicoes da segunda Matriz na primeira condicao 
 		for (int b=0;b<bb;b++) 
 			if(B[b]>4)
 			System.out.printf("\n Numero B nessa possicao: "+B[b]);
 			
 			System.out.print("\n");
 		
+		// criando condicaes da primeira Matriz na segunda condicao.
 		for (int C=0;C<aa;C++)
 			if(A[C]<=5)
 			System.out.printf("\n Numero AC nessa possicao: "+A[C]);
 		
 			System.out.print("\n");
 		
+		// criando condicoes da segunda matriz para segunda condicao.
 		for (int c=0;c<bb;c++)
 			if(B[c]<=5)
 			System.out.printf("\n Numero AC nessa possicao: "+B[c]);
